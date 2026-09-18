f6447b385cfd171e6b51dfeba74fd9c795c62c39
r3yjell <r3yjell@users.noreply.github.com>
2026-09-18 21:35:46 +0300

    Fix SafeAreas overlay: remove click blocking background, add Escape key exit, fix lock mode

--- FloatNote-Windows/src/Views/SafeAreasWindow.xaml.cs
+++ FloatNote-Windows/src/Views/SafeAreasWindow.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 using FloatNote.Utils;
 
 namespace FloatNote.Views;
@@ -6,6 +7,7 @@ namespace FloatNote.Views;
 public partial class SafeAreasWindow : Window
 {
     private bool _isLocked = false;
+    private bool _is916 = true;
 
     public SafeAreasWindow()
     {
@@ -16,11 +18,37 @@ public partial class SafeAreasWindow : Window
     {
         _isLocked = !_isLocked;
         Win32Helper.SetClickThrough(this, _isLocked);
-        LockBtn.Content = _isLocked ? "🔓 Unlocked" : "🔒 Lock Click";
+        LockBtn.Content = _isLocked ? "🔓 Разблокировать" : "🔒 Сквозной клик";
+    }
+
+    private void Mode_Click(object sender, RoutedEventArgs e)
+    {
+        _is916 = !_is916;
+        VerticalGuide.Visibility = _is916 ? Visibility.Visible : Visibility.Collapsed;
+        HorizontalGuide.Visibility = _is916 ? Visibility.Collapsed : Visibility.Visible;
+        ModeBtn.Content = _is916 ? "Режим: 9:16" : "Режим: 16:9";
     }
 
     private void Close_Click(object sender, RoutedEventArgs e)
     {
+        CloseOverlay();
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Escape)
+        {
+            CloseOverlay();
+        }
+    }
+
+    private void CloseOverlay()
+    {
+        if (_isLocked)
+        {
+            Win32Helper.SetClickThrough(this, false);
+            _isLocked = false;
+        }
         Hide();
     }
 }
